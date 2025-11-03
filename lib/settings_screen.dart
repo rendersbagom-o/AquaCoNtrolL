bd8dc0bd550fd86acc74858f1fafb6049c786b91
disappointtt <sagimbekovd@list.ru>
2025-11-03 21:14:30 +0500

    Added navigation between screens

---
+++ lib/settings_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Настройки"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          "Экран настроек",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
